c695c2232b1732183cccf2c26e59f7387545a9a7
g-cesar <g-cesar1@hotmail.com>
2020-03-05 17:50:49 +0100

    unity

---
+++ Unity/Bounce/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+ 
+ public class MainMenu: MonoBehaviour {
+ 
+      public void SinglePlayer()
+      {
+        SceneManager.LoadScene(1);
+      }
+
+      public void MultiPlayer()
+      {
+        Debug.Log("Not implemented yet...");
+      }
+
+      public void QuitGame()
+      {
+        Application.Quit();
+        Debug.Log("Game is exiting...");
+      }
+ }
