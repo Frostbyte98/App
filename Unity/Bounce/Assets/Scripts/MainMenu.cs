e8c06dae86ae7b4c0d0dfecb17d88eefd000f767
g-cesar <g-cesar1@hotmail.com>
2020-03-06 00:01:36 +0100

    prova touch

--- Unity/Bounce/Assets/Scripts/MainMenu.cs
+++ Unity/Bounce/Assets/Scripts/MainMenu.cs
@@ -1,22 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
- 
- public class MainMenu: MonoBehaviour {
- 
-      public void SinglePlayer()
-      {
-        SceneManager.LoadScene(1);
-      }
+using System;
 
-      public void MultiPlayer()
+public class MainMenu: MonoBehaviour {
+
+
+    bool loading = false;
+
+    public void LoadScene(int index)
+    {
+        if (!loading)
+        {
+            StartCoroutine(LoadSceneDelay(index));
+        }
+    }
+
+    IEnumerator LoadSceneDelay(int index)
+    {
+        loading = true;
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(index);
+    }
+
+    public void QuitApp()
+    {
+        if (!loading)
+        {
+            StartCoroutine(QuitAppDelay());
+        }
+    }
+
+    IEnumerator QuitAppDelay()
+    {
+        loading = true;
+        yield return new WaitForSeconds(2);
+        Application.Quit();
+    }
+    
+    
+    
+    public void SinglePlayer()
+      {
+        LoadScene(1);
+    }
+
+    public void MultiPlayer()
       {
         Debug.Log("Not implemented yet...");
       }
 
       public void QuitGame()
       {
-        Application.Quit();
+        QuitApp();
         Debug.Log("Game is exiting...");
       }
  }
---
+++ Unity/Bounce/Assets/Scripts/ValveAnim.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValveAnim : MonoBehaviour
+{
+
+    private Vector3 touchPosition;
+    private Rigidbody2D rb;
+    private Vector3 distanza;
+    private float rotationSpeed = 10f;
+
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            touchPosition.z = 0;
+            distanza = (touchPosition - transform.position);
+            rb.velocity = new Vector2(distanza.x, distanza.y) * rotationSpeed;
+
+            if(touch.phase == TouchPhase.Ended)
+            {
+                rb.velocity = Vector2.zero;
+            }
+        }
+    }
+}
