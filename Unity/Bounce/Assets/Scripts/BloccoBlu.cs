1babde9252e98373b02fcdccba0be3fb7f5e95b3
frostbyte98 <marcoverri98@gmail.com>
2020-03-06 16:25:49 +0100

    no message

---
+++ Unity/Bounce/Assets/Scripts/BloccoBlu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloccoBlu : MonoBehaviour
+{
+    int punteggio = 100;
+    public Collider2D ball;
+    //public PointHero Hero;
+    private Collider2D blocco;
+
+    public GameObject player;
+    private PointHero pointhero;
+
+    void Awake()
+    {
+        pointhero = GetComponent<PointHero>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        blocco = gameObject.GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (blocco.IsTouching(ball))
+        {
+            Destroy(gameObject, 1);
+            player.GetComponent<PointHero>().SetScore(punteggio);
+
+            Debug.Log(player.GetComponent<PointHero>().GetScore());
+        }
+    }
+}
---
+++ Unity/Bounce/Assets/Scripts/PointHero.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+internal class PointHero : MonoBehaviour
+{
+    int punteggio = 0;
+
+    public void SetScore (int value)
+    {
+        punteggio = punteggio + value;
+    }
+
+    public int GetScore() => punteggio;
+}
