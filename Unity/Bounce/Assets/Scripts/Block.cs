2e67500cb74b2f8e7c56176d388c620dee6b5cda
Marco Verri <marcoverri98@gmail.com>
2020-03-06 18:30:30 +0100

    Aggiornamento + apk

--- Unity/Bounce/Assets/Scripts/Block.cs
+++ Unity/Bounce/Assets/Scripts/Block.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Block : MonoBehaviour
 {
@@ -8,13 +9,18 @@ public class Block : MonoBehaviour
     public Rigidbody2D ball;
     private Collider2D col2d;
     private float old_x;
+    public Text scoreText;
+    public int score = 50;
+
+    private ScoreHero ScoreScript;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
         col2d = gameObject.GetComponent<Collider2D>();
 
-
+        
         if (col2d.enabled == false)
         {
             col2d.enabled = true;
@@ -32,12 +38,23 @@ public class Block : MonoBehaviour
 
         if ((col2d.IsTouching(ball.GetComponent<Collider2D>())) && (new_x==old_x))
         {
-            Destroy(gameObject, 10);
+            Destroy(gameObject, 3);
+            //ScoreScript.SetScore(score);
+            //scoreText.text = ScoreScript.ToString();
+
+            player.GetComponent<ScoreHero>().SetScore(score);
+            scoreText.text = player.GetComponent<ScoreHero>().GetScore().ToString();
+
         }
         
         else if ((this.transform.position.y < -1) && (col2d.IsTouching(ball.GetComponent<Collider2D>())))
         {
             Destroy(gameObject, 1);
+            //ScoreScript.SetScore(score);
+            //scoreText.text = ScoreScript.ToString();
+
+            player.GetComponent<ScoreHero>().SetScore(score);
+            scoreText.text = player.GetComponent<ScoreHero>().GetScore().ToString();
         }
 
         old_x = gameObject.transform.position.x;
