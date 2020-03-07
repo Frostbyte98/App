fb3019ced5156ee7f8db5ec26f33b3c5e162ca4f
Marco Verri <marcoverri98@gmail.com>
2020-03-07 10:57:22 +0100

    ogg

---
+++ Unity/Bounce/Assets/Scripts/MoveBall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.gravityScale = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            rb.gravityScale = 1;
+            Camera cam = Camera.main;
+            transform.position = cam.ScreenToWorldPoint(Input.GetTouch(0).position);
+        }
+            
+    }
+}
