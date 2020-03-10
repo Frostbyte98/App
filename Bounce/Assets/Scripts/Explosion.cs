f69da14d2b1a4fe07ce2c66e3a8a614ec87829da
g-cesar <g-cesar1@hotmail.com>
2020-03-10 14:12:34 +0100

    Bounce (menù [missing: selettore difficoltà, livelli e personaggi, pagina iniziale con spiegazione] + 1 livello [missing: texture bordi schermo, moltiplicatore, check palline rosse colpite + audio diverso, grafica di fine livello, animazioni varie])

---
+++ Bounce/Assets/Scripts/Explosion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+
+    /*Questo script è da integrare nello script Blocco.cs prima della funzione Destroy, così da avere nell'ordine: collisione + audio, esplosione + audio, sparizione blocchetto*/
+
+    private Animator anim;
+    private bool explode = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        explode = true;
+        anim.SetTrigger("explode");
+    }
+}
