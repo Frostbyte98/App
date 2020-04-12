93f1fb62dd0ee31ff2f99b2a4d8456e624e073c6
g-cesar <g-cesar1@hotmail.com>
2020-04-13 01:06:58 +0200

    1.0.1

---
+++ Bounce/Assets/Scripts/ScoreboardEntryUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Bounce.Scoreboards
+{
+    public class ScoreboardEntryUI : MonoBehaviour
+    {
+        [SerializeField] private Text entryPositionText = null;
+        [SerializeField] private Text entryNameText = null;
+        [SerializeField] private Text entryScoreText = null;
+
+        public void Initialise(ScoreboardEntryData scoreboardEntryData)
+        {
+            entryPositionText.text = scoreboardEntryData.entryPosition.ToString() + "°";
+            entryNameText.text = scoreboardEntryData.entryName;
+            entryScoreText.text = scoreboardEntryData.entryScore.ToString();
+        }
+    }
+}
